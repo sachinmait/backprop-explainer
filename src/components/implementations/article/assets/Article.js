5ff08dab52215928a16a240001f1257e71c49bff
Dr. Sachin Gupta <sachin.gupta@mait.ac.in>
2025-06-12 15:45:30 +0530

    Create Article.js

---
+++ src/components/implementations/article/assets/Article.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ManualTool from "./ManualTool";
+
+const Explanation = () => (
+  <div style={{ display: "flex", justifyContent: "center" }}>
+    <ManualTool />
+  </div>
+);
+
+export default Explanation;
